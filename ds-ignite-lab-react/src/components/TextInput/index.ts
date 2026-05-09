373ccd9d97e5df65b900dc1378543dec486ff191
Diogo Rocha <diogorocha12@live.com>
2026-05-09 19:33:48 -0300

    delete

--- ds-ignite-lab-react/src/components/TextInput/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./text-input-icon";
-export * from "./text-input-input";
-export * from "./text-input-root";
-export type { TextInputRootProps } from "./text-input-root";
-export type { TextInputInputProps } from "./text-input-input";
-export type { TextInputIconProps } from "./text-input-icon";
